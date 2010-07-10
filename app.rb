6d43af1eed527273b57c74dbe9d2fb026fb2c111
Lorenzo Planas <lplanas@qindio.com>
2010-07-10 21:00:27 +0200

    Switched to Sinatra classic mode so it runs on Heroku

--- app.rb
+++ app.rb
@@ -1,19 +1,14 @@
 # encoding: UTF-8
-require 'sinatra/base'
+require 'sinatra'
 require 'sinatra-charter'
 
-class Charter < Sinatra::Base
-  helpers Sinatra::Charter::Helpers
-
-  set :env,                   :production
-  set :app_file,              __FILE__
-  set :root,                  File.dirname(__FILE__)
-  set :public,                Proc.new { File.join(root, "public") }
-  set :default_locale,        'en'
-  enable                      :static, :show_exceptions
+set :env,                   :production
+set :app_file,              __FILE__
+set :root,                  File.dirname(__FILE__)
+set :public,                Proc.new { File.join(root, "public") }
+set :default_locale,        'en'
+enable                      :static, :show_exceptions
   
 
-  post %r{^/chart/(\w+)\.json/?$} do send_chart_url :from => 'json' end 
-  post %r{^/chart/(\w+)/?$}       do send_chart_url end
-end
-
+post %r{^/chart/(\w+)\.json/?$} do send_chart_url :from => 'json' end 
+post %r{^/chart/(\w+)/?$}       do send_chart_url end
