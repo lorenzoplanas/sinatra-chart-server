c41eff62bfa284b4d81aea084c6ddd35ad656992
Lorenzo Planas <lplanas@qindio.com>
2010-07-10 21:11:45 +0200

    Back to Sinatra application mode

--- app.rb
+++ app.rb
@@ -2,13 +2,15 @@
 require 'sinatra'
 require 'sinatra-charter'
 
-set :env,                   :production
-set :app_file,              __FILE__
-set :root,                  File.dirname(__FILE__)
-set :public,                Proc.new { File.join(root, "public") }
-set :default_locale,        'en'
-enable                      :static, :show_exceptions
-  
+class Charter < Sinatra::Base
+  set :env,                   :production
+  set :app_file,              __FILE__
+  set :root,                  File.dirname(__FILE__)
+  set :public,                Proc.new { File.join(root, "public") }
+  set :default_locale,        'en'
+  enable                      :static, :show_exceptions
 
-post %r{^/chart/(\w+)\.json/?$} do send_chart_url :from => 'json' end 
-post %r{^/chart/(\w+)/?$}       do send_chart_url end
+  post %r{^/chart/(\w+)\.json/?$} do send_chart_url :from => 'json' end 
+  post %r{^/chart/(\w+)/?$}       do send_chart_url end
+
+end
